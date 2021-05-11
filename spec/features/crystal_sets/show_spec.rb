ce698e47cdc7dfc1f9f369483d1732c86aec81b5
Suzie K <suziekim.dev@gmail.com>
2021-05-11 19:27:28 -0400

    Red displays collection name test

---
+++ spec/features/crystal_sets/show_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe 'the crystal set show page' do
+  it 'displays the collection name' do
+  end
+
+  it 'displays limited edition status'
+
+  it 'displays inventory'
+end
