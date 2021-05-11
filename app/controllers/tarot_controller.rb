0bcf36e22feb506335ebd30656b2a2104d05d9f6
Lawrence Whalen <ldwhalen@gmail.com>
2021-05-11 12:00:36 -0600

    Add route and controller method for creating deck

--- app/controllers/tarot_controller.rb
+++ app/controllers/tarot_controller.rb
@@ -5,4 +5,7 @@ class TarotController < ApplicationController
 
   def new
   end
+
+  def create
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/tarot', to: 'tarot#index'
   get '/tarot/new', to: 'tarot#new'
+  post '/tarot', to: 'tarot#create'
 end
