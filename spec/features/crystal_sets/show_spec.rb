3db913b06c98cfd302ce4ba5383d5cb96d1b5ec1
Suzie K <suziekim.dev@gmail.com>
2021-05-12 18:13:31 -0400

    Update crystals/show_spec and test edge case

--- spec/features/crystal_sets/show_spec.rb
+++ spec/features/crystal_sets/show_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 
 RSpec.describe 'the crystal set show page' do
   it 'displays collection and attributes' do
-    set = CrystalSet.create!(collection_name: "Raw Crystal Chunks - 28pc", limited_edition: true, inventory: 3)
-    set_1 = CrystalSet.create!(collection_name: "Moonlight ", limited_edition: false, inventory: 10)
-    visit "/crystal_sets/#{set.id}"
+    set_1 = CrystalSet.create!(collection_name: "Raw Crystal Chunks - 28pc", limited_edition: true, inventory: 3)
+    set_2 = CrystalSet.create!(collection_name: "Moonlight ", limited_edition: false, inventory: 10)
+    visit "/crystal_sets/#{set_1.id}"
     save_and_open_page    
 
-    expect(page).to have_content(set.collection_name)
-    expect(page).to have_content(set.limited_edition)
-    expect(page).to have_content(set.inventory)
-    expect(page).to_not have_content(set_1.collection_name)
+    expect(page).to have_content(set_1.collection_name)
+    expect(page).to have_content(set_1.limited_edition)
+    expect(page).to have_content(set_1.inventory)
+    expect(page).to_not have_content(set_2.collection_name)
   end
 end
--- spec/features/crystals/show_spec.rb
+++ spec/features/crystals/show_spec.rb
@@ -2,25 +2,25 @@
 require 'rails_helper'
 
 RSpec.describe 'the crystal show page' do
-  it 'displays crystal and attributes' do
-    set = CrystalSet.create!(collection_name: "Raw Crystal Chunks - 28pc", limited_edition: true, inventory: 3)
-    crystal = set.crystals.create!(name: "Larimar", price: 4.55, charged: true, description: "Peace/Relaxation/Communication")
-    visit "/crystals/#{crystal.id}"
+  before :each do
+    @set = CrystalSet.create!(collection_name: "Raw Crystal Chunks - 28pc", limited_edition: true, inventory: 3)
+    @larimar = @set.crystals.create!(name: "Larimar", price: 4.55, charged: true, description: "Peace/Relaxation/Communication")
+    @emerald = @set.crystals.create!(name: "Emerald", price: 100.99, charged: true, description: "Love/Compassion/Abundance")
+  end
 
-    expect(page).to have_content(crystal.name)
-    expect(page).to have_content(crystal.price)
-    expect(page).to have_content(crystal.charged)
-    expect(page).to have_content(crystal.description)
+  it 'displays crystal and attributes' do
+    visit "/crystals/#{@larimar.id}"
+
+    expect(page).to have_content(@larimar.name)
+    expect(page).to have_content(@larimar.price)
+    expect(page).to have_content(@larimar.charged)
+    expect(page).to_not have_content(@emerald.name)
   end
 
   it 'displays crystal set crystal belongs to' do
-    set = CrystalSet.create!(collection_name: "Raw Crystal Chunks - 28pc", limited_edition: true, inventory: 3)
-    crystal = set.crystals.create!(name: "Larimar", price: 4.55, charged: true, description: "Peace/Relaxation/Communication")
-    visit "/crystals/#{crystal.id}"
-
-    expect(page).to have_content(crystal.name)
-    expect(page).to have_content(crystal.price)
-    expect(page).to have_content(crystal.charged)
-    expect(page).to have_content(crystal.description)
+    visit "/crystals/#{@larimar.id}"
+  
+    expect(page).to have_content(@larimar.crystal_set.collection_name)
+    expect(page).to_not have_content(@emerald.crystal_set)
   end
 end
