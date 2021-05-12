549776e87546d50e2eaef0de4d2e69de8cdbbffe
Suzie K <suziekim.dev@gmail.com>
2021-05-11 23:43:17 -0400

    Red this test displays crystal and attributes

--- spec/features/crystal_sets/show_spec.rb
+++ spec/features/crystal_sets/show_spec.rb
@@ -1,9 +1,10 @@
+#spec/features/crystal_sets/show_spec.rb
 require 'rails_helper'
 
 RSpec.describe 'the crystal set show page' do
   it 'displays collection and attributes' do
-    set = CrystalSet.create(collection_name: "Raw Crystal Chunks - 28pc", limited_edition: true, inventory: 3)
-    set_1 = CrystalSet.create(collection_name: "Moonlight ", limited_edition: false, inventory: 10)
+    set = CrystalSet.create!(collection_name: "Raw Crystal Chunks - 28pc", limited_edition: true, inventory: 3)
+    set_1 = CrystalSet.create!(collection_name: "Moonlight ", limited_edition: false, inventory: 10)
     visit "/crystal_sets/#{set.id}"
     save_and_open_page    
 
