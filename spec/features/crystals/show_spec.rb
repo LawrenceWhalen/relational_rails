1a665e859e33040d4da0cb2a2af1d285d939775f
Suzie K <suziekim.dev@gmail.com>
2021-05-11 23:44:41 -0400

    Red this test for displays crystal and attributes

---
+++ spec/features/crystals/show_spec.rb
@@ -0,0 +1,15 @@
+#spec/features/crystals/show_spec.rb
+require 'rails_helper'
+
+RSpec.describe 'the crystal show page' do
+  it 'displays crystal and attributes' do
+    set = CrystalSet.create!(collection_name: "Raw Crystal Chunks - 28pc", limited_edition: true, inventory: 3)
+    crystal = set.crystals.create!(name: "Larimar", price: 4.55, charged: true, description: "Peace/Relaxation/Communication")
+    visit "/crystals/#{crystal.id}"
+
+    expect(page).to have_content(crystal.name)
+    expect(page).to have_content(crystal.price)
+    expect(page).to have_content(crystal.charged)
+    expect(page).to have_content(crystal.description)
+  end
+end
