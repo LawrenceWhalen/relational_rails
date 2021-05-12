66b94e14e5c6573dd16a8211d8e79995066ed2c8
Suzie K <suziekim.dev@gmail.com>
2021-05-11 22:51:47 -0400

    Add route for show.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/crystal_sets', to: 'crystal_sets#index'
+  get '/crystal_sets/:id', to:'crystal_sets#show'
   get '/crystals', to: 'crystals#index'
 end
