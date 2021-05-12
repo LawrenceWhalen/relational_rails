a2ef764bd6b5b928791396d3343595eb8184383a
Suzie K <suziekim.dev@gmail.com>
2021-05-11 22:53:46 -0400

    Green this test for display collection and attributes

--- spec/features/crystal_sets/show_spec.rb
+++ spec/features/crystal_sets/show_spec.rb
@@ -1,10 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe 'the crystal set show page' do
-  it 'displays the collection name' do
+  it 'displays collection and attributes' do
+    set = CrystalSet.create(collection_name: "Raw Crystal Chunks - 28pc", limited_edition: true, inventory: 3)
+    set_1 = CrystalSet.create(collection_name: "Moonlight ", limited_edition: false, inventory: 10)
+    visit "/crystal_sets/#{set.id}"
+    save_and_open_page    
+
+    expect(page).to have_content(set.collection_name)
+    expect(page).to have_content(set.limited_edition)
+    expect(page).to have_content(set.inventory)
+    expect(page).to_not have_content(set_1.collection_name)
   end
-
-  it 'displays limited edition status'
-
-  it 'displays inventory'
 end
