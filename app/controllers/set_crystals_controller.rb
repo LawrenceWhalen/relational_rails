5c4f790c9de9f5333e92d26621dc669d8a577192
Suzie K <suziekim.dev@gmail.com>
2021-05-12 21:22:52 -0400

    Green this test for crystals belonging to crystal set

---
+++ app/controllers/set_crystals_controller.rb
@@ -0,0 +1,8 @@
+#app/controllers/set_crystals_controller 
+
+class SetCrystalsController < ApplicationController
+  def index
+    @set = CrystalSet.find(params[:crystal_set_id])
+    @crystals = set.crystals 
+  end
+end
