efcb1e9db85016e20eb700c832ffc6748a878ada
Suzie K <suziekim.dev@gmail.com>
2021-05-15 22:29:54 -0400

    Red this test for links to each crystal set show

--- spec/features/crystal_sets/index_spec.rb
+++ spec/features/crystal_sets/index_spec.rb
@@ -26,9 +26,16 @@ RSpec.describe 'crystal set index' do
     expect(full_set_name_1).to appear_before(full_set_name_2, only_text: true)
   end
 
-  it 'links to crystals index page' do\
+  it 'links to crystals index page' do
     visit '/crystal_sets'
     
     expect(page).to have_link("All Crystals", href: '/crystals')
+  end
+
+  it 'links to each sets show page' do
+    visit '/crystal_sets'
+
+    expect(page).to have_link("#{@set_1.collection_name}", href: "/crystal_sets/#{@set_1.id}")
+    expect(page).to have_link("#{@set_2.collection_name}", href: "/crystal_sets/#{@set_2.id}")
   end
 end
