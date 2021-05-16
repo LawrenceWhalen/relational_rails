a2c2f310bee5bd9a25f090150433acf1a00f2d1d
Suzie K <suziekim.dev@gmail.com>
2021-05-16 10:29:31 -0400

    Minor edits

--- spec/models/crystal_set_spec.rb
+++ spec/models/crystal_set_spec.rb
@@ -8,9 +8,10 @@ RSpec.describe CrystalSet do
       @set = CrystalSet.create!(collection_name: "Raw Crystal Chunks - 28pc", limited_edition: true, inventory: 3)
       @larimar = @set.crystals.create!(name: "Larimar", price: 4.55, charged: true, description: "Peace/Relaxation/Communication")
       @emerald = @set.crystals.create!(name: "Emerald", price: 100.99, charged: true, description: "Love/Compassion/Abundance")
-    end 
+    end
 
-    it 'returns number of crystals in crystal set' do 
+    it 'returns number of crystals in crystal set' do
+
       expect(@set.crystal_count).to eq(2)
     end
   end
