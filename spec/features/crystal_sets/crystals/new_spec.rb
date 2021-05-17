eebe06dcc5b14a5223e03a56a080dc4ec303ff45
Suzie K <suziekim.dev@gmail.com>
2021-05-17 16:40:07 -0400

    Red ttest for can create new crystal

---
+++ spec/features/crystal_sets/crystals/new_spec.rb
@@ -0,0 +1,29 @@
+#spec/features/crystal_sets/crystals/new
+require 'rails_helper'
+
+RSpec.describe 'Crystal Set Crystal creation' do
+  before :each do 
+    @set = CrystalSet.create!(collection_name: "5 Different Rough Stones", limited_edition: false , inventory: 40) 
+  end
+
+  it 'links to the new page from set crystal index' do
+    visit "/crystal_sets/#{@set.id}/crystals"
+    click_link "Add Crystal"
+    expect(current_path).to eq("/crystal_sets/#{@set.id}/crystals/new")
+  end
+
+  it 'can create a new crystal' do
+    visit "/crystal_sets/#{@set.id}/crystals/new"
+
+    expect(current_path).to eq("/crystal_sets/#{@set.id}/crystals/new")
+
+    fill_in "Name", with: "Amethyst"
+    fill_in "Price", with:  12.99
+    uncheck "Charged?"
+    fill_in "Description", with: "Stress Relief/Protection"
+    click_button("Create Crystal")
+
+    expect(current_path).to eq("/crystal_sets/#{@set.id}/crystals")
+  end
+
+end
