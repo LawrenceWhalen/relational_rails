2a24c2aaf9eb3fe5f6a7a284d42776a6862e6974
Suzie K <suziekim.dev@gmail.com>
2021-05-16 21:08:01 -0400

    Red this test for can edit crystal set

--- spec/features/crystal_sets/edit_spec.rb
+++ spec/features/crystal_sets/edit_spec.rb
@@ -10,5 +10,19 @@ RSpec.describe 'Crystal sets edit' do
 
     expect(current_path).to eq("/crystal_sets/#{set.id}")
   end
-  
+
+  it 'can edit the crystal set' do
+    set = CrystalSet.create!(collection_name: "5 Different Rough Stones", limited_edition: false , inventory: 40) 
+    visit "/crystal_sets"
+
+    expect(page).to have_content("5 Different Rough Stones")
+
+    click_button "Edit #{set.id}"
+
+    fill_in 'Collection Name', with: "4 Different Rough Stones"
+    click_button 'Update Set'
+
+    expect(current_path).to eq("/crystal_sets")
+    expect(page).to have_content("4 Different Rough Stones")
+  end
 end
