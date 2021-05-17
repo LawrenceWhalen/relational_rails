451a2eb3265fc04470663ccc63bf20d9a3500b7f
Suzie K <suziekim.dev@gmail.com>
2021-05-16 20:58:00 -0400

    Green this test for links to edit page

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/crystal_sets', to: 'crystal_sets#index'
   get '/crystal_sets/new', to: 'crystal_sets#new'
+  post '/crystal_sets', to: 'crystal_sets#create'
+  get '/crystal_sets/:id/edit', to: 'crystal_sets#edit'
+  post '/crystal_sets/:id', to: 'crystal_sets#edit'
   get '/crystal_sets/:id', to:'crystal_sets#show'
-  get '/crystal_sets/:crystal_set_id/crystals', to: 'set_crystals#index'
-  
+  get '/crystal_sets/:id/crystals', to: 'set_crystals#index'
+
   get '/crystals', to: 'crystals#index'
   get '/crystals/:id', to: 'crystals#show'
 end
