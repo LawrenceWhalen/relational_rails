485103057712d240af0d77235faac9e785387ca9
Lawrence Whalen <ldwhalen@gmail.com>
2021-05-16 20:19:48 -0600

    Green user story 11 tests

--- spec/features/tarot_decks/new_spec.rb
+++ spec/features/tarot_decks/new_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe 'the Tarot Deck creation' do
 
     page.should have_selector("[value='Register New Deck']")
 
-    click_link('New Artist')
+    click_button('Register New Deck')
 
     expect(current_path).to eq('/tarot_decks/new')
   end
@@ -14,11 +14,11 @@ RSpec.describe 'the Tarot Deck creation' do
     visit '/tarot_decks/new'
     fill_in('Deck Name', with: 'Hunters')
     fill_in('Number Made', with: 6)
-    fill_in('Pre-Owned', with: true)
+    check('Pre-Owned')
     click_button('Add Deck')
 
     new_deck_id = TarotDeck.last.id
-    expect(current_path).to eq("/tarot_decks/#{new_deck_id}")
+    expect(current_path).to eq("/tarot_decks")
     expect(page).to have_content("Hunters")
   end
 end
