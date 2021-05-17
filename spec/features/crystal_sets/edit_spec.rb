51cb72cda826a692939378ed9bfef6ac75e0d499
Suzie K <suziekim.dev@gmail.com>
2021-05-16 20:57:39 -0400

    Red this test for links to edit page

---
+++ spec/features/crystal_sets/edit_spec.rb
@@ -0,0 +1,13 @@
+#/spec/features/crystal_sets/edit_spec
+require 'rails_helper'
+
+RSpec.describe 'Crystal sets edit' do 
+  it 'links to the edit page' do
+    set = CrystalSet.create!(collection_name: "5 Different Rough Stones", limited_edition: false , inventory: 40) 
+    visit "/crystal_sets"
+
+    click_button "Edit #{set.id}"
+
+    expect(current_path).to eq("/crystal_sets/edit")
+  end
+end
