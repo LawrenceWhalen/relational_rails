4a53b99301f9027e1d0548425ed96265df0a5c87
Suzie K <suziekim.dev@gmail.com>
2021-05-18 18:06:19 -0400

    Red test for can delete crystal set

---
+++ spec/features/crystal_sets/destroy_spec.rb
@@ -0,0 +1,14 @@
+#spec/features/crsystal_sets/destroy
+
+require 'rails_helper'
+
+RSpec.describe 'destroying a crystal set' do
+  it 'can delete the crystal set from the index page' do
+    set = CrystalSet.create!(collection_name: "Moon Charged Stones", )
+
+    visit '/crystal_sets'
+    click_button 'Delete'
+
+    expect(current_path).to eq('/crystal_sets')
+  end
+end
