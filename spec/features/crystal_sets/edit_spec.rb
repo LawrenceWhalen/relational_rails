d14d484ea564885c5aff1e68c6c675ccb0312929
Suzie K <suziekim.dev@gmail.com>
2021-05-17 20:38:30 -0400

    Add space

--- spec/features/crystal_sets/edit_spec.rb
+++ spec/features/crystal_sets/edit_spec.rb
@@ -21,7 +21,7 @@ RSpec.describe 'Crystal sets edit' do
     click_button "Edit #{@set.collection_name}"
  
     fill_in 'Collection Name', with: "4 Different Rough Stones"
-    click_button'Update Crystal Set'
+    click_button 'Update Crystal Set'
 
     expect(current_path).to eq("/crystal_sets")
     expect(page).to have_content("4 Different Rough Stones")
