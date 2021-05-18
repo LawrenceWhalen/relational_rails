02890a8daa2a2204b0c75648c4c2015569919568
Suzie K <suziekim.dev@gmail.com>
2021-05-18 18:44:12 -0400

    Make minor edits

--- app/models/crystal_set.rb
+++ app/models/crystal_set.rb
@@ -4,7 +4,7 @@ class CrystalSet < ApplicationRecord
   has_many :crystals
 
   def self.order_by_most_recently_created
-    self.all.order(:created_at).reverse_order 
+    self.order(:created_at).reverse_order 
   end
 
   def crystal_count
