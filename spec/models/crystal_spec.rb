bfe13cd17b95ad118f282232275c7ed5a8fb6eea
Suzie K <suziekim.dev@gmail.com>
2021-05-18 09:43:39 -0400

    Red test for show_only_true method

--- spec/models/crystal_spec.rb
+++ spec/models/crystal_spec.rb
@@ -2,4 +2,19 @@ require 'rails_helper'
 
 RSpec.describe Crystal do
   it {should belong_to :crystal_set}
+
+  describe "methods" do
+    before :each do 
+      @set = CrystalSet.create!(collection_name: "Starter Set 4 Stones", limited_edition: false, inventory: 100)
+      @larimar = @set.crystals.create!(name: "Larimar", price: 4.55, charged: true, description: "Peace/Relaxation/Communication")
+      @emerald = @set.crystals.create!(name: "Emerald", price: 100.99, charged: true, description: "Love/Compassion/Abundance")
+      @amethyst= @set.crystals.create!(name: "Ametheyst", price: 12.99, charged: false, description: "Stress Relief/Protection")
+    end
+
+    it '::show_only_true'
+
+      expect(Crystal.show_only_tue).to eq([@larimar, @emerald]) 
+    end
+
+  end
 end
