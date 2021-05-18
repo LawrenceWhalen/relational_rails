5bbff136f1b4bcf1ea6449edd680be8a262aeea9
Lawrence Whalen <ldwhalen@gmail.com>
2021-05-18 15:03:12 -0600

    Red user story 15 test

--- spec/features/readings/index_spec.rb
+++ spec/features/readings/index_spec.rb
@@ -1,7 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe 'the Reading index' do
-  before :each do
+  it 'displays all the Readings and their informations' do
+    @deck = TarotDeck.create!(
+      name: 'Programs',
+      number_made: 15,
+      pre_owned: false
+      )
+
+    @reading_1 = @deck.readings.create!(
+      spread: 'Three Card',
+      major_reading: true,
+      cards_drawn: 'Empress Fool',
+      attendees_num: 6,
+      interpretation: 'I am doomed'
+    )
+    @reading_2 = @deck.readings.create!(
+      spread: 'Celtic Cross',
+      major_reading: true,
+      cards_drawn: 'Six of Wands Seven of Pentacles',
+      attendees_num: 1,
+      interpretation: 'Looks good'
+    )
+
+    visit "/readings"
+
+    expect(page).to have_content(@reading_1.spread)
+    expect(page).to have_content(@reading_1.major_reading)
+    expect(page).to have_content(@reading_1.cards_drawn)
+    expect(page).to have_content(@reading_1.attendees_num)
+    expect(page).to have_content(@reading_1.interpretation)
+    expect(page).to have_content(@reading_2.spread)
+    expect(page).to have_content(@reading_2.major_reading)
+    expect(page).to have_content(@reading_2.cards_drawn)
+    expect(page).to have_content(@reading_2.attendees_num)
+    expect(page).to have_content(@reading_2.interpretation)
+  end
+  it 'displays only readings with a true boolean' do
     @deck = TarotDeck.create!(
       name: 'Programs',
       number_made: 15,
@@ -22,8 +57,6 @@ RSpec.describe 'the Reading index' do
       attendees_num: 1,
       interpretation: 'Looks good'
     )
-  end
-  it 'displays all the Readings and their informations' do
 
     visit "/readings"
 
@@ -32,10 +65,10 @@ RSpec.describe 'the Reading index' do
     expect(page).to have_content(@reading_1.cards_drawn)
     expect(page).to have_content(@reading_1.attendees_num)
     expect(page).to have_content(@reading_1.interpretation)
-    expect(page).to have_content(@reading_2.spread)
-    expect(page).to have_content(@reading_2.major_reading)
-    expect(page).to have_content(@reading_2.cards_drawn)
-    expect(page).to have_content(@reading_2.attendees_num)
-    expect(page).to have_content(@reading_2.interpretation)
+    expect(page).to_not have_content(@reading_2.spread)
+    expect(page).to_not have_content(@reading_2.major_reading)
+    expect(page).to_not have_content(@reading_2.cards_drawn)
+    expect(page).to_not have_content(@reading_2.attendees_num)
+    expect(page).to_not have_content(@reading_2.interpretation)
   end
 end
