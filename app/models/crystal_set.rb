00002089e4cfb3633d2a4eb29849f8fbe531aa03
Suzie K <suziekim.dev@gmail.com>
2021-05-19 09:26:55 -0400

    Add dependent: :delete_all logic

--- app/models/crystal_set.rb
+++ app/models/crystal_set.rb
@@ -1,7 +1,7 @@
 #app/models/crystal_set
 
 class CrystalSet < ApplicationRecord
-  has_many :crystals
+  has_many :crystals, dependent: :delete_all
 
   def self.order_by_most_recently_created
     order(:created_at).reverse_order 
