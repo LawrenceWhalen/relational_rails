a986fb77830e6847b912eea0b72b37a9cf92866c
Suzie K <suziekim.dev@gmail.com>
2021-05-19 08:50:03 -0400

    Clean up methods

--- app/models/crystal_set.rb
+++ app/models/crystal_set.rb
@@ -4,7 +4,7 @@ class CrystalSet < ApplicationRecord
   has_many :crystals
 
   def self.order_by_most_recently_created
-    self.order(:created_at).reverse_order 
+    order(:created_at).reverse_order 
   end
 
   def crystal_count
@@ -12,9 +12,6 @@ class CrystalSet < ApplicationRecord
   end
 
   def shows_crystals_pricier_than(price)
-    if price.nil?
-      
-    end
-    self.crystals.where("price > #{price}")
+    crystals.where("price > #{price}")
   end
 end
