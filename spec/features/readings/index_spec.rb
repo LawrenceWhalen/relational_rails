5b6d91786c01e87e75ac77dca71cd84a7cd529df
Lawrence Whalen <ldwhalen@gmail.com>
2021-05-18 18:45:49 -0600

    Red user story 18 test

--- spec/features/readings/index_spec.rb
+++ spec/features/readings/index_spec.rb
@@ -71,4 +71,34 @@ RSpec.describe 'the Reading index' do
     expect(page).to_not have_content(@reading_2.attendees_num)
     expect(page).to_not have_content(@reading_2.interpretation)
   end
+  it 'has an edit button on the view page' do
+    deck = TarotDeck.create!(
+      name: 'Programs',
+      number_made: 15,
+      pre_owned: false,
+    )
+
+    reading_1 = deck.readings.create(
+      spread: 'Three Card',
+      major_reading: true,
+      cards_drawn: 'Empress Fool',
+      attendees_num: 6,
+      interpretation: 'I am doomed'
+    )
+    reading_2 = deck.readings.create(
+      spread: 'Hello',
+      major_reading: true,
+      cards_drawn: 'Maji',
+      attendees_num: 1,
+      interpretation: 'we will live'
+    )
+
+    visit "/readings"
+
+    expect(page).to have_selector("[value='Update Reading ##{reading_1.id}']")
+
+    click_button('Update Reading')
+
+    expect(current_path).to eq("/readings/#{reading_1.id}/edit")
+  end
 end
