ae7bd73c10bb24272c363fd5f0bb8e61d8798a9f
Lawrence Whalen <ldwhalen@gmail.com>
2021-05-18 22:50:35 -0600

    Green user story 23 test

--- spec/features/readings/index_spec.rb
+++ spec/features/readings/index_spec.rb
@@ -2,20 +2,20 @@ require 'rails_helper'
 
 RSpec.describe 'the Reading index' do
   it 'displays all the Readings and their informations' do
-    @deck = TarotDeck.create!(
+    deck = TarotDeck.create!(
       name: 'Programs',
       number_made: 15,
       pre_owned: false
       )
 
-    @reading_1 = @deck.readings.create!(
+    reading_1 = deck.readings.create!(
       spread: 'Three Card',
       major_reading: true,
       cards_drawn: 'Empress Fool',
       attendees_num: 6,
       interpretation: 'I am doomed'
     )
-    @reading_2 = @deck.readings.create!(
+    reading_2 = deck.readings.create!(
       spread: 'Celtic Cross',
       major_reading: true,
       cards_drawn: 'Six of Wands Seven of Pentacles',
@@ -25,32 +25,32 @@ RSpec.describe 'the Reading index' do
 
     visit("/readings")
 
-    expect(page).to have_content(@reading_1.spread)
-    expect(page).to have_content(@reading_1.major_reading)
-    expect(page).to have_content(@reading_1.cards_drawn)
-    expect(page).to have_content(@reading_1.attendees_num)
-    expect(page).to have_content(@reading_1.interpretation)
-    expect(page).to have_content(@reading_2.spread)
-    expect(page).to have_content(@reading_2.major_reading)
-    expect(page).to have_content(@reading_2.cards_drawn)
-    expect(page).to have_content(@reading_2.attendees_num)
-    expect(page).to have_content(@reading_2.interpretation)
+    expect(page).to have_content(reading_1.spread)
+    expect(page).to have_content(reading_1.major_reading)
+    expect(page).to have_content(reading_1.cards_drawn)
+    expect(page).to have_content(reading_1.attendees_num)
+    expect(page).to have_content(reading_1.interpretation)
+    expect(page).to have_content(reading_2.spread)
+    expect(page).to have_content(reading_2.major_reading)
+    expect(page).to have_content(reading_2.cards_drawn)
+    expect(page).to have_content(reading_2.attendees_num)
+    expect(page).to have_content(reading_2.interpretation)
   end
   it 'displays only readings with a true boolean' do
-    @deck = TarotDeck.create!(
+    deck = TarotDeck.create!(
       name: 'Programs',
       number_made: 15,
       pre_owned: false
       )
 
-    @reading_1 = @deck.readings.create!(
+    reading_1 = deck.readings.create!(
       spread: 'Three Card',
       major_reading: true,
       cards_drawn: 'Empress Fool',
       attendees_num: 6,
       interpretation: 'I am doomed'
     )
-    @reading_2 = @deck.readings.create!(
+    reading_2 = deck.readings.create!(
       spread: 'Celtic Cross',
       major_reading: false,
       cards_drawn: 'Six of Wands Seven of Pentacles',
@@ -60,16 +60,16 @@ RSpec.describe 'the Reading index' do
 
     visit("/readings")
 
-    expect(page).to have_content(@reading_1.spread)
-    expect(page).to have_content(@reading_1.major_reading)
-    expect(page).to have_content(@reading_1.cards_drawn)
-    expect(page).to have_content(@reading_1.attendees_num)
-    expect(page).to have_content(@reading_1.interpretation)
-    expect(page).to_not have_content(@reading_2.spread)
-    expect(page).to_not have_content(@reading_2.major_reading)
-    expect(page).to_not have_content(@reading_2.cards_drawn)
-    expect(page).to_not have_content(@reading_2.attendees_num)
-    expect(page).to_not have_content(@reading_2.interpretation)
+    expect(page).to have_content(reading_1.spread)
+    expect(page).to have_content(reading_1.major_reading)
+    expect(page).to have_content(reading_1.cards_drawn)
+    expect(page).to have_content(reading_1.attendees_num)
+    expect(page).to have_content(reading_1.interpretation)
+    expect(page).to_not have_content(reading_2.spread)
+    expect(page).to_not have_content(reading_2.major_reading)
+    expect(page).to_not have_content(reading_2.cards_drawn)
+    expect(page).to_not have_content(reading_2.attendees_num)
+    expect(page).to_not have_content(reading_2.interpretation)
   end
   it 'has an edit button on the view page' do
     deck = TarotDeck.create!(
@@ -127,7 +127,9 @@ RSpec.describe 'the Reading index' do
 
     visit("/readings")
 
-    expect(page).to have_selector("[value='Delete Reading ##{reading_1.id}']")
+    within('li#0') do
+      expect(page).to have_selector("[value='Delete Reading ##{reading_1.id}']")
+    end
 
     click_button("Delete Reading ##{reading_1.id}")
 
