d4698c6d46667424f9893763e37023a47b55f288
Lawrence Whalen <ldwhalen@gmail.com>
2021-05-18 22:35:11 -0600

    Red user story 23 test

--- spec/features/readings/index_spec.rb
+++ spec/features/readings/index_spec.rb
@@ -103,4 +103,37 @@ RSpec.describe 'the Reading index' do
 
     expect(current_path).to eq("/readings/#{reading_1.id}/edit")
   end
+  it 'has buttons to delete the readings' do
+    deck = TarotDeck.create!(
+      name: 'Programs',
+      number_made: 15,
+      pre_owned: false,
+    )
+
+    reading_1 = deck.readings.create(
+      spread: 'Three Card',
+      major_reading: true,
+      cards_drawn: 'Empress Fool',
+      attendees_num: 6,
+      interpretation: 'I am doomed'
+    )
+    reading_2 = deck.readings.create(
+      spread: 'Hello',
+      major_reading: true,
+      cards_drawn: 'Maji',
+      attendees_num: 1,
+      interpretation: 'we will live'
+    )
+
+    visit("/readings")
+
+    expect(page).to have_selector("[value='Delete Reading ##{reading_1.id}']")
+
+    click_button("Delete Reading ##{reading_1.id}")
+
+    expect(current_path).to eq("/readings")
+
+    expect(page).to have_content('Hello')
+    expect(page).to_not have_content('Three Card')
+  end
 end
